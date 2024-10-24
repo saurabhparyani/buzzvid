0f45d75085dbb1de616c15972655757236792893
Saurabh Paryani <saurabhparyani64@gmail.com>
2024-10-25 00:04:10 +0530

    changed frontend url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://buzz-ruby.vercel.app',
     credentials: true,
     allowedHeaders: [
       'Accept',
